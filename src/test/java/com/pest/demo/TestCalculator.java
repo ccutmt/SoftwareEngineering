5e4495cecf5367bd3c2bce21b9b4f048a3e1f87e
ccutmt <christophercutajar@gmail.com>
2014-04-09 12:49:57 +0200

    first commit

---
+++ src/test/java/com/pest/demo/TestCalculator.java
@@ -0,0 +1,42 @@
+package com.pest.demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestCalculator {
+
+	Calculator calc;
+	
+	@Before
+	public void setUp() throws Exception {
+		calc = new Calculator();
+	}
+
+	@Test
+	public void testAddPositiveNumbers() {
+		assertEquals(25, calc.add(20, 5));
+	}
+	
+	@Test
+	public void testAddNegativeNumbers() {
+		assertEquals(-27, calc.add(-20, -7));
+	}
+
+	@Test
+	public void testSubtraction() {
+		assertEquals(5, calc.sub(10, 5));
+	}
+	
+	@Test
+	public void testMultiplication() {
+		assertEquals(25, calc.multiply(5, 5));
+	}
+	
+	@Test
+	public void testDivision() {
+		assertEquals(6, calc.divide(30, 5));
+	}
+
+}
