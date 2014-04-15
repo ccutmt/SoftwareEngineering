7f155fb9f94ab39a5ea4b928530763dd4859c387
borgmatthew <borgmb@gmail.com>
2014-04-15 14:32:39 +0200

    Verify map size

--- src/main/java/com/pest/demo/Map.java
+++ src/main/java/com/pest/demo/Map.java
@@ -4,9 +4,14 @@ public class Map {
 	static int size;
 	
 	boolean setMapSize(int x, int y){
-		if(x < 2 || x > 50)
+		if(x > 50)
+			return false;
+		else if(Game.no_players <= 4 && x < 5)
+			return false;
+		else if(Game.no_players <= 8 && x < 8)
 			return false;
 		else size = x;
+		generate();
 		return true;
 	}
 	
