e9ecce7d56f51e6f85bdaa0a7b94a35d1ba48cea
borgmatthew <borgmb@gmail.com>
2014-04-16 00:31:29 +0200

    testing tests

--- src/test/java/com/pest/demo/GameTest.java
+++ src/test/java/com/pest/demo/GameTest.java
@@ -1,46 +1,21 @@
 package com.pest.demo;
 
 import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class GameTest {
 
-	Game game;
-
+	Game mygame;
 	@Before
 	public void setUp() throws Exception {
-		game = new Game();
-	}
-	
-	@Test
-	public void testGame() {
-		//fail("Not yet implemented");
-	}
-
-	@Test
-	public void testMain() {
-		//fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGenerateHTMLFiles() {
-		assertEquals("","");
+		mygame = new Game();
 	}
 
 	@Test
 	public void testSetNumPlayers() {
-		//assertEquals(false, game.setNumPlayers(1));
-	}
-
-	@Test
-	public void testPrint() {
-		//assertEquals("hi", game.print("hi"));
-	}
-
-	@Test
-	public void testPrintln() {
-		assertEquals("hi", "hi");
+		assertEquals(true, mygame.setNumPlayers(2));
 	}
 
 }
