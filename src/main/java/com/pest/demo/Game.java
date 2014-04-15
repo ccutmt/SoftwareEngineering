cce011298510687eb036b221e8eb5449e45988bf
ccutmt <christophercutajar@gmail.com>
2014-04-15 12:17:23 +0200

    testing table

--- src/main/java/com/pest/demo/Game.java
+++ src/main/java/com/pest/demo/Game.java
@@ -18,10 +18,10 @@ public class Game {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        generateHTMLFiles();
+        generatehtmlFiles();
     }
     
-    public static void generateHTMLFiles()
+    public static void generatehtmlFiles()
     {
         try {
 
@@ -29,24 +29,33 @@ public class Game {
             file.createNewFile();
             FileWriter writer = new FileWriter(file);
 
-            writer.write("<HTML>\n");
-            //HEAD
-            writer.write("<HEAD>\n");
-            writer.write("<TITLE> Software Engineering Assignment </TITLE>\n");
-            writer.write("</HEAD>\n");
+            writer.write("<html>\n");
+            //head
+            writer.write("<head>\n");
+            writer.write("<title> Software Engineering Assignment </title>\n");
+            writer.write("</head>\n");
             
-            //BODY
-            writer.write("<BODY>\n");
+            //body
+            writer.write("<body>\n");
             
-            //FORM
-            int tiles = 0;
-            writer.write("<FORM>\n");
-            writer.write("Number of tiles: <input type=\"text\" name=\"tiles\"><br>\n");
-            writer.write("</FORM>\n");
             
-            writer.write("</BODY>\n");
+            //table
+            writer.write("<table style=\"width:300px\">");
+            for (int i = 0; i < 3; i++) {   
+                writer.write("<tr>");
+                writer.write("<td>");
+                for (int j = 0; j < 3; j++) {
+                    writer.write("*");
+                }
+
+                writer.write("</td>");
+                writer.write("</tr>");
+            }
+            writer.write("</table>");
             
-            writer.write("</HTML>\n");
+            writer.write("</body>\n");
+            
+            writer.write("</html>\n");
             
             /*Scanner sc = new Scanner(System.in);
             System.out.print("Enter an integer ");
