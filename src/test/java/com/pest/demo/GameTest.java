e7c894a90c713dc4ad8ffdb748c37259bcfdcf6d
borgmatthew <borgmb@gmail.com>
2014-04-16 01:05:06 +0200

    Set up most of Game class tests

--- src/test/java/com/pest/demo/GameTest.java
+++ src/test/java/com/pest/demo/GameTest.java
@@ -1,17 +1,27 @@
 package com.pest.demo;
 
 import static org.junit.Assert.*;
+import java.io.*;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.After;
 
 public class GameTest {
 
 	Game mygame;
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	
 	@Before
 	public void setUp() throws Exception {
 		mygame = new Game();
 	}
+	
+	@Before
+	public void setUpStreams() {
+	    System.setOut(new PrintStream(outContent));
+	}
+
 
 	@Test
 	public void testSetNumPlayers() {
@@ -19,6 +29,23 @@ public class GameTest {
 		assertEquals(true, mygame.setNumPlayers(8));
 		assertEquals(false, mygame.setNumPlayers(9));
 		assertEquals(false, mygame.setNumPlayers(1));
+	}
+	
+	@Test
+	public void testPrint(){
+		mygame.print("test");
+		assertEquals("test", outContent.toString());
+	}
+	
+	@Test
+	public void testPrintln(){
+		mygame.print("test");
+		assertEquals("test", outContent.toString());
+	}
+	
+	@After
+	public void cleanUpStreams() {
+	    System.setOut(null);
 	}
 
 }
