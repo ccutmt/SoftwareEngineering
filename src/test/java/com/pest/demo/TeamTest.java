d163dd0cafd768cf6b54d22ef8cc26c888470ef7
ccutmt <christophercutajar@gmail.com>
2014-05-27 16:04:35 +0200

    TeamTest.java

---
+++ src/test/java/com/pest/demo/TeamTest.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pest.demo;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class TeamTest {
+    
+}
